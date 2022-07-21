2fda4d9443da94b7fe3ff157c6335faa5f17765b
naimulislamshakil <ms0413354@gmail.com>
2022-07-21 09:06:34 +0600

    add mongodb

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmanv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+console.log("db connet");
 
 app.get("/", (req, res) => {
   res.send("How are you?");
